282e49130711054973692db8085f6bce971bedc3
ioanh <ioan.xristov@abv.bg>
2021-03-12 13:51:24 +0200

    recipe dummy test

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe [] = [
+    new Recipe('Test recipe', 'This is the description', 'https://upload.wikimedia.org/wikipedia/commons/7/7a/African_recipes.jpg')
+  ];
 
   constructor() { }
 
