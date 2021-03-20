360529e5d3507e6ef09b9ffe15fbb92a52df5f8e
ioanh <ioan.xristov@abv.bg>
2021-03-20 16:22:38 +0200

    ingredient
    implementing the ingredient model with a test

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,10 @@
+export class Ingredient {
+    // public name: string;
+    // public ammount: number;
+
+    // constructor(name: string, ammount: number){
+    //     this.name = name;
+    //     this.ammount = ammount;
+    // }
+    constructor(public name: string, public amount: string){}
+}
