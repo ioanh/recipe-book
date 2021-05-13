937ddb028e06f4657a980874c43bab4b7f76b7b9
ioanh <ioan.xristov@abv.bg>
2021-05-14 01:26:08 +0300

    implementing the logic and making it work

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
 
-  constructor() { }
+  clicked: boolean = false;
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+  @HostListener("click") mouseClick(){
+    this.clicked = !this.clicked;
+    if(this.clicked){
+      this.renderer.addClass(this.elRef.nativeElement,"open");
+    }else{
+      this.renderer.removeClass(this.elRef.nativeElement, "open");
+    }
+  }
 
 }
