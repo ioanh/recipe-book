f0d00952d7820df03a386ace4a02d5a59147fa16
ioanh <ioan.xristov@abv.bg>
2021-06-04 00:12:54 +0300

    Creating shopping list and recipes services

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListSerivce } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
+  providers: [ShoppingListSerivce]
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [
