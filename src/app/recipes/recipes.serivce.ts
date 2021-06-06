e46c6041e9883d4c8fee24eea1ace2b80869bbc7
ioanh <ioan.xristov@abv.bg>
2021-06-07 00:13:28 +0300

    adding ingredients to the Recipes and showing them

--- src/app/recipes/recipes.serivce.ts
+++ src/app/recipes/recipes.serivce.ts
@@ -1,12 +1,13 @@
 import {EventEmitter} from '@angular/core'
+import { Ingredient } from '../shared/ingredient.model';
 import {Recipe} from './recipe.model'
 export class RecipesService{
 
     recipeSelected = new EventEmitter<Recipe>(); 
     
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('A Test Cunt', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [new Ingredient('Meat', 20)]),
+        new Recipe('A Test Cunt', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [new Ingredient('Meat', 20)])
       ];
 
     getRecipes() {
