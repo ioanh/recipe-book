58e3edd017852f26f78925c8b011216c735eada0
ioanh <ioan.xristov@abv.bg>
2021-08-03 21:36:52 +0300

    finalizing the router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
     {path: '', redirectTo: 'recipes', pathMatch: 'full'},
     {path: 'recipes', component: RecipesComponent, children: [
-        {path: ':id'}
+        {path: ':id', component: RecipeItemComponent}
     ]},
     {path: 'shopping-list', component: ShoppingListComponent}
 ]
