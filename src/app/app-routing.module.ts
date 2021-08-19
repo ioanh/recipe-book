a20255670deba98134600ed11b9e9738443df18a
ioanh <ioan.xristov@abv.bg>
2021-08-19 11:57:21 +0300

    adding it to the router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
+import { RecipesDefaultComponent } from "./recipes/recipes-default/recipes-default.component";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
     {path: '', redirectTo: 'recipes', pathMatch: 'full'},
     {path: 'recipes', component: RecipesComponent, children: [
+        {path: '', component: RecipesDefaultComponent},
         {path: ':id', component: RecipeDetailComponent},
     ]},
     {path: 'shopping-list', component: ShoppingListComponent}
