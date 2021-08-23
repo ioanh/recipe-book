d54a676207f017c419d1ddf47acbf94ae7d0b361
ioanh <ioan.xristov@abv.bg>
2021-08-23 15:53:12 +0300

    fixing the router to show hardcode routes, before dynamic ones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,8 +11,8 @@ const appRoutes: Routes = [
     {path: '', redirectTo: 'recipes', pathMatch: 'full'},
     {path: 'recipes', component: RecipesComponent, children: [
         {path: '', component: RecipesDefaultComponent},
-        {path: ':id', component: RecipeDetailComponent},
         {path: 'new', component: RecipeEditComponent},
+        {path: ':id', component: RecipeDetailComponent},
         {path: ':id/edit', component: RecipeEditComponent}
     ]},
     {path: 'shopping-list', component: ShoppingListComponent}
