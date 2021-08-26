46e3907c1c78a271f85f0542245eb3f6afa4cbbf
ioanh <ioan.xristov@abv.bg>
2021-08-26 04:17:24 +0300

    getting the selected recipe from the router for the edit component HTML

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Recipe } from '../recipe.model';
+import { RecipesService } from '../recipes.serivce';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -8,14 +10,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class RecipeEditComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private recipesService: RecipesService) { }
 
   allowEdit = false;
   id: number;
+  recipe: Recipe;
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.id = +params['id']
+      this.recipe = this.recipesService.getRecipe(this.id)
       this.allowEdit = params['id'] != null
     })
   }
